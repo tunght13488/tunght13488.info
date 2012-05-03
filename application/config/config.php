9750781c24f44abca76c4966dab7de1373bbf6f2
Ha The Tung <tunght13488@gmail.com>
2012-05-04 02:51:43 +0700

    config

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '{*0LN(_-G4p(iU(<}.{0zq73e=f%JSCC';
 
 /*
 |--------------------------------------------------------------------------
